3116391c87cfe4f964ca71d7f76e9152d6790b0b
Sabith <mohamedsabith2@gmail.com>
2024-07-15 15:50:45 +0530

    Sign Out implemented

--- app/src/main/java/com/shatrend/parkx/fragments/customer/ProfileFragment.java
+++ app/src/main/java/com/shatrend/parkx/fragments/customer/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.shatrend.parkx.fragments.customer;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +8,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.shatrend.parkx.R;
+import com.shatrend.parkx.activities.customer.CustomerLoginActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -30,15 +35,6 @@ public class ProfileFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ProfileFragment newInstance(String param1, String param2) {
         ProfileFragment fragment = new ProfileFragment();
         Bundle args = new Bundle();
@@ -60,7 +56,19 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        Button btnSignOut = view.findViewById(R.id.btn_sign_out);
+        btnSignOut.setOnClickListener(v -> {
+            // Sign out from Firebase
+            FirebaseAuth.getInstance().signOut();
+            Toast.makeText(getActivity(), "Signed Out", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(getActivity(), CustomerLoginActivity.class);
+            startActivity(intent);
+            getActivity().finish();
+        });
+
+        return view;
     }
 }
